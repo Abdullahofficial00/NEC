196a8a2469b81e3365f7cdbaed428067894a5d82
Rayan-Rasheed <rasheedrayan514@gmail.com>
2023-11-24 01:48:26 +0500

    Update SignUp:Thisi commit update a SignUp page

--- react_frontend/client/src/App.js
+++ react_frontend/client/src/App.js
@@ -1,12 +1,23 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import SignInForm from './components/Login/LoginMain/SignInForm';
 
 function App() {
   return (
     <>
-      <div className="bg-dark">
-        <h1 className="text-success">hello</h1>
-      </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/login1' element={<SignInForm/>}/>
+
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   );
 }
