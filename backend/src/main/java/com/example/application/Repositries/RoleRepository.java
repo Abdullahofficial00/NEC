2cbfbc6ea36ebf3482a1beda9c3e0797a8aa73da
Rayan-Rasheed <rasheedrayan514@gmail.com>
2023-11-25 22:38:11 +0500

    Revert "feat(Oppointment):Add appointment functionality with login"
    This reverts commit 734b1c1e1d5fd7da7c82cbb2873f14fc4d39306e.

--- backend/src/main/java/com/example/application/Repositries/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.application.Repositries;
-
-import com.example.application.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Role findByName(String roleName);
-}
--- react_frontend/client/src/App.js
+++ react_frontend/client/src/App.js
@@ -4,11 +4,7 @@ import './App.css';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Dashboard from './components/Dashboard/Dashboard/Dashboard';
 import SignInForm from './pages/Login/LoginMain/SignInForm';
-import AddDoctor from './components/Dashboard/AddDoctor/AddDoctor';
-import DoctorList from './components/Dashboard/DoctorList/DoctorList';
-import AddReview from './components/Dashboard/My Review/AddReview';
 
 function App() {
   return (
@@ -18,8 +14,6 @@ function App() {
         <Route path='/' element={<Homepage/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/login1' element={<SignInForm/>}/>
-        <Route path='/login1' element={<Dashboard/>}/>
-        
 
         <Route path='/register' element={<Register/>}/>
       </Routes>
