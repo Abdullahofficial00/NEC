26bc497e7c95429b18259bc28944fc05f6c7f58b
Rayan <rasheedrayan514@gmail.com>
2023-11-25 12:06:58 +0500

    Update: Fix Structures

--- spring_backend/src/main/java/com/example/application/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.application.services;
-import com.example.application.dto.AccountDto;
-import com.example.application.entities.*;
-public interface UserService {
-    AccountDto createUser(AccountDto account);
-    Account findAccountById(Integer id);
-    Account findAccountByEmail(String email);
-    Account findAccountByPhoneNumber(String number);
-}
